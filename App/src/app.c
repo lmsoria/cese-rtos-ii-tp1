411bc654c22a88a8ab63bf04b8551c8d77d67242
Leandro Soria <leandrosoria@hotmail.com>
2023-11-06 17:13:31 -0300

    Added app directory

---
+++ App/src/app.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "cmsis_os.h"
+
+#include "app.h"
+#include "app_resources.h"
+
+/// | Private typedef -----------------------------------------------------------
+/// | Private define ------------------------------------------------------------
+/// | Private macro -------------------------------------------------------------
+/// | Private function prototypes -----------------------------------------------
+/// | Private variables ---------------------------------------------------------
+/// | Exported variables -------------------------------------------------------
+
+/// | Private functions ---------------------------------------------------------
+
+void app_init()
+{
+    printf("Main application starts here\n");
+}
