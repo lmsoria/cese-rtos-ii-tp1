4255ded7b304b7184090d9ac642ac0e684ef0dc6
Leandro Soria <leandrosoria@hotmail.com>
2023-11-07 16:09:03 -0300

    Implemented a debouncing mechanism

--- SVC/src/SVC_button.c
+++ SVC/src/SVC_button.c
@@ -7,7 +7,27 @@
 #include "SVC_button.h"
 
 /// | Private typedef -----------------------------------------------------------
+
+typedef enum
+{
+	DEBOUNCER_STATE_WAIT_PRESS = 0,    ///< Key released, waiting to be pressed
+	DEBOUNCER_STATE_DEBOUNCE_ACTIVE,   ///< The key has been pressed and we need to filter the debouncing
+	DEBOUNCER_STATE_WAIT_RELEASE,      ///< Key pressed, waiting to be released
+	DEBOUNCER_STATE_DEBOUNCE_INACTIVE, ///< The key has been released, so we need to filter the debouncing
+} DebouncerState;
+
+typedef struct
+{
+	BoardButtons button;
+	DebouncerState state;
+    uint32_t timer_debounce;
+    uint32_t timer_up;
+} Debouncer;
+
 /// | Private define ------------------------------------------------------------
+
+#define DEBOUNCE_PERIOD_MS 40
+
 /// | Private macro -------------------------------------------------------------
 /// | Private variables ---------------------------------------------------------
 /// | Private function prototypes -----------------------------------------------
@@ -15,6 +35,62 @@
 /// | Private functions ---------------------------------------------------------
 void task_button(void* parameters)
 {
+	ButtonTaskData* const DATA = (ButtonTaskData*)(parameters);
+
+	Debouncer debouncer =
+	{
+		.button = DATA->button,
+		.state = DEBOUNCER_STATE_WAIT_PRESS,
+		.timer_debounce = 0,
+		.timer_up = 0,
+	};
+
+
 	printf("[%s] Task Created\n", pcTaskGetName(NULL));
-	while(1) {}
+	while(1) {
+		switch(debouncer.state) {
+		case DEBOUNCER_STATE_WAIT_PRESS:
+			if(button_read(debouncer.button) == BUTTON_PRESSED) {
+				debouncer.timer_debounce = pdMS_TO_TICKS(DEBOUNCE_PERIOD_MS);
+				debouncer.state = DEBOUNCER_STATE_DEBOUNCE_ACTIVE;
+			}
+			break;
+		case DEBOUNCER_STATE_DEBOUNCE_ACTIVE:
+			if(button_read(debouncer.button) == BUTTON_RELEASED) {
+				debouncer.state = DEBOUNCER_STATE_WAIT_PRESS;
+			} else if(debouncer.timer_debounce) {
+				debouncer.timer_debounce--;
+			} else {
+				debouncer.timer_up = 0;
+				debouncer.state = DEBOUNCER_STATE_WAIT_RELEASE;
+			}
+			break;
+		case DEBOUNCER_STATE_WAIT_RELEASE:
+			if(button_read(debouncer.button) == BUTTON_RELEASED) {
+				debouncer.timer_debounce = pdMS_TO_TICKS(DEBOUNCE_PERIOD_MS);
+				debouncer.state = DEBOUNCER_STATE_DEBOUNCE_INACTIVE;
+			}
+			break;
+		case DEBOUNCER_STATE_DEBOUNCE_INACTIVE:
+			if(button_read(debouncer.button) == BUTTON_PRESSED) {
+				debouncer.state = DEBOUNCER_STATE_WAIT_RELEASE;
+			} else if(debouncer.timer_debounce) {
+				debouncer.timer_debounce--;
+			} else {
+				debouncer.state = DEBOUNCER_STATE_WAIT_PRESS;
+			}
+			break;
+		default:
+			debouncer.state = DEBOUNCER_STATE_WAIT_PRESS;
+			break;
+		}
+
+		if(debouncer.state == DEBOUNCER_STATE_WAIT_RELEASE) {
+			debouncer.timer_up++;
+		} else {
+			debouncer.timer_up = 0;
+		}
+
+		vTaskDelay(pdMS_TO_TICKS(1));
+	}
 }
