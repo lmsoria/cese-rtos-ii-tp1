febacca215275b2c22b081fe73ae08e4e29b8dff
Leandro Soria <leandrosoria@hotmail.com>
2023-11-28 16:12:12 -0300

    More doxy

--- LIB/inc/event.h
+++ LIB/inc/event.h
@@ -5,9 +5,9 @@
 /// @brief 
 typedef void (*dispatch_event_handler_t)(uint32_t, void*);
 
-/// @brief 
+/// @brief Common structure that represents the data passed to a task when an event occurs 
 typedef struct
 {
-	uint32_t id;
-	void* opt_data_address;
+	uint32_t id;            ///< The ID of the event, defined by the services.
+	void* opt_data_address; ///< Optional data defined by the event.
 } Event;
