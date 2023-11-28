5b48387475ffb8e3ffe1017f4e6043435bbaa65a
Leandro Soria <leandrosoria@hotmail.com>
2023-11-28 14:10:43 -0300

    Dynamic AO implementation

--- LIB/inc/dyn_ao.h
+++ LIB/inc/dyn_ao.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "task.h"
+
+#include "event.h"
+
+#define DYN_AO_MAX_TASKS_COUNT 5
+#define DYN_AO_TASK_NAME_LENGHT 16
+
+typedef struct {
+    QueueHandle_t queue;
+    TaskHandle_t* task[DYN_AO_MAX_TASKS_COUNT];
+    dispatch_event_handler_t dispatch_function;
+    size_t task_count;
+    size_t client_count;
+    char task_name[DYN_AO_TASK_NAME_LENGHT];
+} DynamicAO;
+
+bool dynamic_ao_initialize(DynamicAO* const ao, const char* task_name, dispatch_event_handler_t handler);
+
+
+bool dynamic_ao_send_event(DynamicAO* const ao, Event* const event);
--- LIB/src/dyn_ao.c
+++ LIB/src/dyn_ao.c
@@ -1,1 +1,119 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "dyn_ao.h"
+
+#define DYNAMIC_AO_QUEUE_LENGTH 1
+#define DYNAMIC_AO_TASK_STACK_SIZE (2 * configMINIMAL_STACK_SIZE)
+#define DYNAMIC_AO_TASK_PRIORIY (tskIDLE_PRIORITY + 1UL)
+
+
+static void dynamic_ao_delete_task(DynamicAO* const ao)
+{
+	printf("[DYN AO] Deleting task \"%s\"\n", pcTaskGetName(NULL));
+	ao->task_count--;
+	printf("[DYN AO] Running tasks \"%s\" count: %d\n", ao->task_name, ao->task_count);
+
+	vTaskDelete(NULL);
+}
+
+
+static void ao_task(void* parameters)
+{
+	DynamicAO* const AO = (DynamicAO*)(parameters);
+
+    Event event;
+    printf("[%s] Task Running\n", pcTaskGetName(NULL));
+
+    while(1) {
+        if(xQueueReceive(AO->queue, &event, 0) == pdPASS) {
+            if(AO->dispatch_function) {
+            	AO->dispatch_function(event.id, event.opt_data_address);
+            }
+        } else {
+        	dynamic_ao_delete_task(AO);
+        }
+    }
+}
+
+static bool dynamic_ao_create_task(DynamicAO* const ao)
+{
+	if(ao->task_count < DYN_AO_MAX_TASKS_COUNT) {
+		printf("[DYN AO] Creating task \"%s\"\n", ao->task_name);
+
+		BaseType_t ret = xTaskCreate(
+				ao_task,
+				ao->task_name,
+				DYNAMIC_AO_TASK_STACK_SIZE,
+				(void*) ao,
+				DYNAMIC_AO_TASK_PRIORIY,
+				NULL);
+
+		if(ret != pdPASS) {
+			printf("[DYN AO] Error creating task \"%s\"\n", ao->task_name);
+			return false;
+		}
+
+		ao->task_count++;
+
+		printf("[DYN AO] Tasks \"%s\" count: %d\n", ao->task_name, ao->task_count);
+
+		return true;
+	} else {
+		printf("[DYN AO] Cannot allocate more \"%s\" tasks\n", ao->task_name);
+		return false;
+	}
+
+}
+
+
+bool dynamic_ao_initialize(DynamicAO* const ao, const char* task_name, dispatch_event_handler_t handler)
+{
+    if(ao == NULL) {
+    	printf("[DYN AO] Received NULL AO\n");
+    	return false;
+    }
+
+//    if(handler == NULL) {
+//    	printf("[AO] Received NULL handler\n");
+//    	return false;
+//    }
+
+    ao->dispatch_function = handler;
+    ao->task_count = 0;
+    ao->client_count = 0;
+    memcpy(ao->task_name, task_name, strlen(task_name));
+
+    ao->queue = xQueueCreate(DYNAMIC_AO_QUEUE_LENGTH, sizeof(Event));
+    if(ao->queue == NULL) {
+    	printf("[DYN AO] Error creating queue for task \"%s\"\n", task_name);
+    	return false;
+    }
+
+    printf("[DYN AO] Dynamic Active Object \"%s\" created!\n", task_name);
+    return true;
+}
+
+bool dynamic_ao_send_event(DynamicAO* ao, Event* const event)
+{
+    if (xQueueSend(ao->queue, (void*)(event), 0) == pdPASS) {
+    	printf("[DYN AO] Posted an event to the \"%s\" queue\n", ao->task_name);
+    	if(ao->task_count == 0) {
+    		dynamic_ao_create_task(ao);
+    	}
+    	return true;
+    } else if(dynamic_ao_create_task(ao)) {
+    	printf("[DYN AO] Existing tasks have full queues, created new one\n");
+    	if(xQueueSend(ao->queue, (void*)(event), 0) == pdPASS) {
+    		printf("[DYN AO] Posted an event to the queue\n");
+    		return true;
+    	} else {
+    		printf("[DYN AO] Error posting an event\n");
+    	}
+    } else {
+        printf("[DYN AO] Cannot create more \"%s\" tasks\n", ao->task_name);
+    }
+
+    printf("[DYN AO] Error posting the event to \"%s\"'s queue\n", ao->task_name);
+	return false;
+}
